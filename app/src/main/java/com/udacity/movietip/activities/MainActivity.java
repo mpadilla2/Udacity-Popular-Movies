6b4e929161861c4897fb9707934d06b9cc476c1b
Melissa Padilla <mpadilla2@gmail.com>
2018-05-19 09:05:02 -0600

    Initial Commit

---
+++ app/src/main/java/com/udacity/movietip/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.udacity.movietip.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacity.movietip.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
