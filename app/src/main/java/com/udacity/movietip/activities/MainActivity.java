3c149320ea6740c7afd93a051335dd71886d1925
Melissa Padilla <mpadilla2@gmail.com>
2018-05-21 05:31:57 -0600

    added notes for upcoming todos

--- app/src/main/java/com/udacity/movietip/activities/MainActivity.java
+++ app/src/main/java/com/udacity/movietip/activities/MainActivity.java
@@ -18,6 +18,42 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    DONE Generify the Interface methods. Test for OK response.
+    TODO Add a recycler view. Test for Ok response.
+    TODO Refactor to fragments. Test for ok response in each fragment.
+    TODO Display data in recyclerview gridview.
+    TODO Respond to click with toast message.
+    TODO Design details fragment.
+    TODO On click display details activity.
+
+    Inside the fragment layout, fragment_movie_grid, I will define the recycler view.
+Inside the MovieGridFragment, I will
+	inflate the fragment_movie_grid
+	get a reference to the recycler view inside the fragment layout
+	add a click listener to handle clicks on the individual posters
+	use parcelable to create a parcel to use in saving state
+	override onSaveInstanceState to save state for each fragment's parcel
+Inside the MainActivity:
+	check for a saved instance state, if it exists: unpack the parcel and restore the savedInstanceState objects
+		else instantiate a MovieGridFragment for each: popular, top rated, and favorites
+		instantiated fragment passes static final string to recycler view to obtain data
+Inside the interface
+	if the passed in static final string matches then (have abstracted and provided methods for popular and toprated)
+		query the api
+Inside the Recycler View class:
+	Abstract so each fragment can instantiate their own recycler view.
+	Connect to the interface to query for the data to display by passing the static final string to the interface
+Lesson 7 and 12 Background Tasks talks about loaders rewatch these and implement the loading here
+	Exercise: T05b.02-Exercise-AddAsyncTaskLoader
+
+1. Create a layout that defines the appearance of the fragment
+2. Create a new fragment class that inflates a layout
+main activity layout has to have a container for the fragment
+3. Create a new fragment instance in a host activity
+4. Add the fragment to it's host activity using the FragmentManager and a fragment transaction
+     */
+
     private ApiService mService;
     private TextView mTextView;
 
